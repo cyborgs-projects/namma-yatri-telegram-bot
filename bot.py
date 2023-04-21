6b0584794fb46039727513ab4013fcce31ea14bc
Aditya Thorat <thorat.aditya@outlook.com>
2023-04-21 20:08:47 +0530

    intial commit

---
+++ bot.py
@@ -0,0 +1,28 @@
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from telegram import Update
+import logging
+
+
+import os
+BOT_TOKEN = os.environ.get('BOT_TOKEN')
+
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="I'm a bot, please talk to me!"
+    )
+
+if __name__ == '__main__':
+    application = ApplicationBuilder().token(BOT_TOKEN).build()
+
+    start_handler = CommandHandler('start', start)
+    application.add_handler(start_handler)
+
+    application.run_polling()
