8a60e6e4361aeec6197e6d200e692292eb10687f
darshan9405 <narendarbhandari9188@gmail.com>
2023-04-21 23:09:13 +0530

    add git-ignore

--- bot.py
+++ bot.py
@@ -1,36 +1,47 @@
 import logging
 from telegram import Update
-from telegram.ext import filters, MessageHandler, ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import (
+    filters,
+    MessageHandler,
+    ApplicationBuilder,
+    CommandHandler,
+    ContextTypes,
+)
 
+from dotenv import load_dotenv
 
+load_dotenv()
 import os
-BOT_TOKEN = os.environ.get('BOT_TOKEN')
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
 
 logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!"
+    )
 
 
 # async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    # await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+# await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
 
 async def caps(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    text_caps = ' '.join(context.args).upper()
+    text_caps = " ".join(context.args).upper()
     await context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     application = ApplicationBuilder().token(BOT_TOKEN).build()
 
-    start_handler = CommandHandler('start', start)
+    start_handler = CommandHandler("start", start)
     # echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
-    caps_handler = CommandHandler('caps', caps)
+    caps_handler = CommandHandler("caps", caps)
 
     application.add_handler(start_handler)
     # application.add_handler(echo_handler)
